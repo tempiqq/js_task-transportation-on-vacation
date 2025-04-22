4f90662f5e20bc4611ad0549c4da967f8ad9d7fb
Roman Osadko <osadko03@gmail.com>
2025-04-22 17:28:32 +0300

    add letter2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,17 +6,17 @@
 function calculateRentalCost(days) {
   const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
-  const LONG_TERM_DICOUNT = 50;
+  const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
-  const SHORT_TERM_DICOUNT = 20;
+  const SHORT_TERM_DISCOUNT = 20;
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return basePrice - LONG_TERM_DICOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return basePrice - SHORT_TERM_DICOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
